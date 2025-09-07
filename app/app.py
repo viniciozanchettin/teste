b256e931bd29a183766df6912638c5e382f12ac8
viniciozanchettin <vinicio@valeti.inf.br>
2025-09-07 20:11:54 -0300

    Change

--- app/app.py
+++ app/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 
 def create_app():
     app = Flask(__name__)
-    app.config.update(SECRET_KEY="change-me")
+    app.config.update(SECRET_KEY="change-me now")
 
     @app.get("/")
     def index():
